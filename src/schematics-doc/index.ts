7d1add9f16716a304549582b60eb3db2d219a861
PastaBolo <alain.paolini29>
2019-01-24 16:10:30 +0100

    initial commit

--- src/schematics-doc/index.ts
+++ src/schematics-doc/index.ts
@@ -1,10 +1,48 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, externalSchematic } from '@angular-devkit/schematics'
+import {
+  addPackageJsonDependency,
+  NodeDependency,
+  NodeDependencyType
+} from '@schematics/angular/utility/dependencies'
+import { RunSchematicTask, NodePackageInstallTask } from '@angular-devkit/schematics/tasks'
 
+export function addUIJarDependency(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const uijarDependency: NodeDependency = {
+      name: 'ui-jar',
+      version: '*',
+      type: NodeDependencyType.Dev
+    }
+    addPackageJsonDependency(tree, uijarDependency)
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function schematicsDoc(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+    if (0) context.addTask(new NodePackageInstallTask())
+  }
+}
+
+export function generateUIJarProject(): Rule {
+  return externalSchematic('@schematics/angular', 'application', {
+    name: 'app-test',
+    skipInstall: true,
+    minimal: true
+  })
+}
+
+export function setupUIJarProject(): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+    console.log('coucou')
+  }
+}
+
+export default function schematicsDoc(_options: any): Rule {
+  return (_tree: Tree, context: SchematicContext) => {
+    context.addTask(new RunSchematicTask('add-uijar-dependency', {}))
+
+    // const installTaskId = context.addTask(new RunSchematicTask('add-uijar-dependency', {}))
+
+    // const projectCreationTaskId = context.addTask(new RunSchematicTask('generate-uijar-app', {}), [
+    //   installTaskId
+    // ])
+
+    // context.addTask(new RunSchematicTask('setup-uijar-app', {}), [projectCreationTaskId])
+  }
 }
