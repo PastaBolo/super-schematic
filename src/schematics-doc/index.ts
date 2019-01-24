de4618221a2eb4b319065f5d4ce61aa90d742418
Alain P <alain.paolini291@gmail.com>
2019-01-24 22:49:45 +0100

    update

--- src/schematics-doc/index.ts
+++ src/schematics-doc/index.ts
@@ -6,40 +6,50 @@ import {
   mergeWith,
   apply,
   url,
-  move
+  move,
+  chain,
+  noop
 } from '@angular-devkit/schematics'
 import {
   addPackageJsonDependency,
   NodeDependency,
   NodeDependencyType
 } from '@schematics/angular/utility/dependencies'
-// import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks'
-import { RunSchematicTask, NodePackageInstallTask } from '@angular-devkit/schematics/tasks'
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks'
 
-/////////////////////// https://github.com/briebug/jest-schematic
+export default function(options: any): Rule {
+  options.skipInstall = true
 
-export function addUIJarDependency(): Rule {
-  return (tree: Tree, context: SchematicContext) => {
+  return chain([
+    (_tree: Tree, context: SchematicContext) => {
+      !options.skipInstall ? context.addTask(new NodePackageInstallTask()) : noop()
+    },
+    externalSchematic('@schematics/angular', 'application', {
+      name: 'ui-jar',
+      skipInstall: options.skipInstall,
+      minimal: true
+    }),
+    addUIJarDependency(),
+    setupUIJarProject()
+  ])
+}
+
+function addUIJarDependency(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
     const uijarDependency: NodeDependency = {
       name: 'ui-jar',
       version: '*',
       type: NodeDependencyType.Dev
     }
     addPackageJsonDependency(tree, uijarDependency)
-
-    if (0) context.addTask(new NodePackageInstallTask())
   }
 }
 
-export function generateUIJarProject(): Rule {
-  return externalSchematic('@schematics/angular', 'application', {
-    name: 'ui-jar',
-    skipInstall: true,
-    minimal: true
-  })
+function setupUIJarProject(): Rule {
+  return chain([updateTsConfig(), replaceFiles()])
 }
 
-export function setupUIJarProject(): Rule {
+function updateTsConfig(): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const json = JSON.parse(tree.read('./projects/ui-jar/tsconfig.app.json')!.toString('utf-8'))
 
@@ -52,19 +62,13 @@ export function setupUIJarProject(): Rule {
       : (json.include = ['**/*.ts', '../../src/**/*.ts'])
 
     tree.overwrite('./projects/ui-jar/tsconfig.app.json', JSON.stringify(json, null, 2))
-
-    tree.delete('./projects/ui-jar/src/index.html')
-
-    return mergeWith(apply(url('./files'), [move('./projects/ui-jar/src')]))
   }
 }
 
-export default function(_options: any): Rule {
-  return (_tree: Tree, context: SchematicContext) => {
-    const installTaskId = context.addTask(new RunSchematicTask('add-uijar-dependency', {}))
-    const projectCreationTaskId = context.addTask(new RunSchematicTask('generate-uijar-app', {}), [
-      installTaskId
-    ])
-    context.addTask(new RunSchematicTask('setup-uijar-app', {}), [projectCreationTaskId])
+function replaceFiles(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    //TODO: Add the main.ts file
+    tree.delete('./projects/ui-jar/src/index.html')
+    mergeWith(apply(url('./files'), [move('./projects/ui-jar/src')]))
   }
 }
